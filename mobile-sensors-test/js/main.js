1b3b90ab1fd733cfcfd85eca66886e2aebde53bc
borivojekovac <borivojekovac@gmail.com>
2022-04-25 17:21:27 +0200

    window.onload instead of document bugfix, plus logging

--- mobile-sensors-test/js/main.js
+++ mobile-sensors-test/js/main.js
@@ -20,41 +20,56 @@ class App {
 
     constructor() {
 
-        console.log(`Accelerometer check: ${typeof(Accelerometer)}`);
-        console.log(`Gyroscope check: ${typeof(Gyroscope)}`);
-        console.log(`LinearAccelerationSensor check: ${typeof(LinearAccelerationSensor)}`);
-        console.log(`AbsoluteOrientationSensor check: ${typeof(AbsoluteOrientationSensor)}`);
-        console.log(`RelativeOrientationSensor check: ${typeof(RelativeOrientationSensor)}`);
-        console.log(`Magnetometer check: ${typeof(Magnetometer)}`);
-        console.log(`THREE check: ${typeof(THREE)}`);
+        this.log(`Accelerometer check: ${typeof(Accelerometer)}`);
+        this.log(`Gyroscope check: ${typeof(Gyroscope)}`);
+        this.log(`LinearAccelerationSensor check: ${typeof(LinearAccelerationSensor)}`);
+        this.log(`AbsoluteOrientationSensor check: ${typeof(AbsoluteOrientationSensor)}`);
+        this.log(`RelativeOrientationSensor check: ${typeof(RelativeOrientationSensor)}`);
+        this.log(`Magnetometer check: ${typeof(Magnetometer)}`);
+        this.log(`THREE check: ${typeof(THREE)}`);
     }
 
     init() {
 
-        this.AccelerometerSensorLabel = document.querySelector("#AccelerometerSensorLabel");
-        this.GyroscopeSensorLabel = document.querySelector("#GyroscopeSensorLabel");
-        this.LinearAccelerationSensorLabel = document.querySelector("#LinearAccelerationSensorLabel");
-        this.AbsoluteOrientationSensorLabel = document.querySelector("#AbsoluteOrientationSensorLabel");
-        this.RelativeOrientationSensorLabel = document.querySelector("#RelativeOrientationSensorLabel");
-        this.MagnetometerSensorLabel = document.querySelector("#MagnetometerSensorLabel");
+        try {
 
-        this.initThree();
-        this.initGeometry();
+            this.log("initializing app...");
 
-        this.initSensor("accelerometer", Accelerometer, this.AccelerometerSensorLabel);
-        this.initSensor("gyroscope", Gyroscope, this.GyroscopeSensorLabel);
-        this.initSensor("linear acceleration", LinearAccelerationSensor, this.LinearAccelerationSensorLabel);
-        this.initSensor("absolute orientation", AbsoluteOrientationSensor, this.AbsoluteOrientationSensorLabel);
-        this.initSensor("relative orientation", RelativeOrientationSensor, this.RelativeOrientationSensorLabel);
-        this.initSensor("magnetometer", Magnetometer, this.MagnetometerSensorLabel);
+            this.AccelerometerSensorLabel = document.querySelector("#AccelerometerSensorLabel");
+            this.GyroscopeSensorLabel = document.querySelector("#GyroscopeSensorLabel");
+            this.LinearAccelerationSensorLabel = document.querySelector("#LinearAccelerationSensorLabel");
+            this.AbsoluteOrientationSensorLabel = document.querySelector("#AbsoluteOrientationSensorLabel");
+            this.RelativeOrientationSensorLabel = document.querySelector("#RelativeOrientationSensorLabel");
+            this.MagnetometerSensorLabel = document.querySelector("#MagnetometerSensorLabel");
+    
+            this.initThree();
+            this.initGeometry();
+    
+            this.initSensor("accelerometer", Accelerometer, this.AccelerometerSensorLabel);
+            this.initSensor("gyroscope", Gyroscope, this.GyroscopeSensorLabel);
+            this.initSensor("linear acceleration", LinearAccelerationSensor, this.LinearAccelerationSensorLabel);
+            this.initSensor("absolute orientation", AbsoluteOrientationSensor, this.AbsoluteOrientationSensorLabel);
+            this.initSensor("relative orientation", RelativeOrientationSensor, this.RelativeOrientationSensorLabel);
+            this.initSensor("magnetometer", Magnetometer, this.MagnetometerSensorLabel);
+
+            return true;
+        }
+        catch (ex) {
+
+            this.log(`Unable to initialize the app: ${JSON.stringify(ex)}`);
+
+            return false;
+        }
     }
 
     initThree() {
 
-        // Create an empty scene
+        this.log("initializing Three.js...").
+
+        // init scene
         this.scene = new THREE.Scene();
 
-        // Create a basic perspective camera
+        // init camera
         this.camera = new THREE.PerspectiveCamera(
             
             75,
@@ -65,45 +80,50 @@ class App {
 
         this.camera.position.z = 4;
 
-        // Create a renderer with Antialiasing
+        // init renderer
         this.renderer = new THREE.WebGLRenderer({
 
             antialias:true
         });
-
-        // Configure renderer clear color
+        
         this.renderer.setClearColor("#000000");
-
-        // Configure renderer size
         this.renderer.setSize( window.innerWidth, window.innerHeight );
 
-        // Append Renderer to DOM
+        // init DOM
         document.body.appendChild( this.renderer.domElement );
     }
 
     initSensor(sensorName, sensorClass, labelElement) {
 
-        console.log(`Initializing ${sensorName}`);
-        
-        var sensor = new sensorClass({ frequency: 60 });
-        sensor.onreading = function() {
+        this.log(`Initializing ${sensorName} sensor...`);
 
-            labelElement.innerText = `${sensorName}: ${JSON.stringify(sensor.quaternion)}`;
-        }
+        try {
 
-        sensor.onerror = function(event) {
+            var sensor = new sensorClass({ frequency: 60 });
 
-            if (event.error.name == 'NotReadableError') {
-
-                console.log(`${sensorName} sensor is not available.`);
-            }
-            else {
+            sensor.onreading = function() {
     
-                console.log(`Unexpected ${sensorName} sensor error.`);
+                labelElement.innerText = `${sensorName}: ${JSON.stringify(sensor.quaternion)}`;
             }
+    
+            sensor.onerror = function(event) {
+    
+                if (event.error.name == 'NotReadableError') {
+    
+                    this.log(`${sensorName} sensor is not available.`);
+                }
+                else {
+        
+                    this.log(`Unexpected ${sensorName} sensor error.`);
+                }
+            }
+    
+            sensor.start();
         }
+        catch (ex) {
 
-        sensor.start();
+            this.log(`Unable to initialize ${sensorName} sensor: ${JSON.stringify(ex)}`);
+        }
     }
 
     heartbeat() {
@@ -115,20 +135,32 @@ class App {
 
     run() {
 
-        console.log("App running.");
+        if (!this.init()) {
 
-        this.init();
+            return;
+        }
+
+        this.log("Initializing heartbeat...")
         this.heartbeat();
+
+        this.log("App running.");
+    }
+
+    log(text) {
+
+        this.log(text);
     }
 
     initGeometry() {
 
-        // Create a Cube Mesh with basic material
+        this.log("Initializing 3D geometry...");
+
+        // create cube
         var geometry = new THREE.BoxGeometry( 1, 1, 1 );
         var material = new THREE.MeshBasicMaterial( { color: "#433F81" } );
         this.cube = new THREE.Mesh( geometry, material );
 
-        // Add cube to Scene
+        // add it to the scene
         this.scene.add( this.cube );
     }
 
@@ -137,11 +169,11 @@ class App {
         this.cube.rotation.x += 0.01;
         this.cube.rotation.y += 0.01;
 
-        // Render the scene
+        // render the scene onto DOM
         this.renderer.render(this.scene, this.camera);
     }
 };
 
 const _app = new App();
 
-document.onload = _app.run;
+window.addEventListener("load", _app.run);
