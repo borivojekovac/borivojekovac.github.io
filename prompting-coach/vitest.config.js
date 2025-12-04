3939fe9d56164ce6208df4ab8f3d6a6c4ca87446
borivojekovac <borivojekovac@gmail.com>
2025-12-04 01:05:26 +0100

    distributable prompting coach

--- prompting-coach/vitest.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vitest/config';
-
-export default defineConfig({
-  test: {
-    exclude: ['**/node_modules/**', '**/tests/e2e/**'],
-    include: ['**/tests/unit/**/*.test.js'],
-  },
-});
